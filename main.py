58c59f25c79ede025b2eee257468d50e617873d5
harsithpro <shoba.harsith@gmail.com>
2024-11-02 13:12:19 +0530

    naruto

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ COLLISION_DISTANCE = 27
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 background = pygame.image.load('Kamui.JPEG')
-playerIMG = pygame.image.load('naruto.jpeg')
+playerImg = pygame.image.load('naruto.jpeg')
 playerX = PLAYER_START_X
 playerY = PLAYER_START_Y
 playerX_change = 0
@@ -31,5 +31,54 @@ for _i in range(num_of_enemies):
     enemyX_change.append(ENEMY_SPEED_X)
     enemyY_change.append(ENEMY_SPEED_Y)
 bulletImg = pygame.image.load("Rasenshuriken.webp")
+bulletX=0
+bulletY=PLAYER_START_Y
+bulletX_change=0
+bulletY_change=BULLET_SPPED_Y
+bullet_state = "ready"
 score_value = 0
-font = 
+font = pygame.font.Font('freesansbold.ttf',32)
+textX = 10
+textY = 10
+over_font = pygame.font.Font('freesansbold.ttf',64)
+def show_score(x, y):
+    score = font.render("Score: " + str(score_value),True,(255, 255, 255))
+    screen.blit(score, (x, y))
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(over_text, (200,250))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state=True
+    screen.blit(bulletImg, (x+16,y+10))
+def iscollision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt((enemyX-bulletX)**2+(enemyY-bulletY)**2)
+    return distance<COLLISION_DISTANCE
+running = True
+while running:
+    screen.fill((0,0,0))
+    screen.blit(background, (0,0))
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+        if event.type==pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -5
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 5
+            if event.key == pygame.K_SPACE and bullet_state == "ready":
+                bulletX = playerX
+                fire_bullet(bulletX, bulletY)
+        if event.type == pygame.KEYUP and event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
+            playerX_change = 0
+    playerX = playerX+playerX_change
+    playerX = max(0, min(playerX, SCREEN_WIDTH - 64))
+    for i in range(num_of_enemies):
+        if enemyY[i] > 340:
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000
+            game_over_text()
